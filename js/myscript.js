1c166095041f9f06ff77af65cf368c93c59df78d
Rahul Sen <rsen253@gmail.com>
2016-07-18 11:15:33 +0530

    Modified Homepage layout

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,4 @@
 $(function () {
-
-    //"use strict";
-
-    //var topoffset = 50; //variable for menu height
-
-    ////Activate Scrollspy
-    //$('body').scrollspy({
-    //    target: 'header .navbar',
-    //    offset: topoffset
-    //});
-
-    //var hash = $(this).find('li.active a').attr('href');
-    //if (hash !== '#featured') {
-    //    $('header nav').addClass('inbody');
-    //} else {
-    //    $('header nav').removeClass('inbody');
-    //}
-
-    //console.log();
-    //// Add an inbody class to nav when scrollspy event fires
-    //$('.navbar-fixed-top').on('activate.bs.scrollspy', function () {
-    //    var hash = $(this).find('li.active a').attr('href');
-    //    if (hash !== '#featured') {
-    //        $('header nav').addClass('inbody');
-    //    } else {
-    //        $('header nav').removeClass('inbody');
-    //    }
-    //});
     var scrolled_val;
     $(window).scroll(function () {
         scrolled_val = $(document).scrollTop().valueOf();
@@ -39,8 +11,53 @@ $(function () {
         }
 
     });
+
+    //Automatic carousel indicator
+    var SlideNumber = $('#featured .item').length;
+    for (var i = 0; i < SlideNumber; i++) {
+        var indicator;
+        if (i == 0) {
+           indicator = '<li data-target="#featured" class="active" data-slide-to="' + i + '"></li>';
+        }
+        else {
+           indicator = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
+        }
+        
+        $('#featured ol').append(indicator);
+    }
+    //Adjust height automaticaly
+    var windowheight = $(window).height();
+    $('.full-screen').css('height', windowheight);
+    $('#featured .item img').each(function () {
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css({ 'background-image': 'url(' + imgSrc + ')' });
+        $(this).remove();
+    });
+    $(window).resize(function () {
+        windowheight = $(window).height();
+        $('.full-screen').css('height', windowheight);
+    });
+
+
+    var eventHeight = $('.recent-events .row').height();
+    $('.recent-events .join-now').css('height', eventHeight);
+
     $('.carousel').carousel({
         interval: false
     });
 
+
+    // init Isotope
+    var $container = $('.work-items').isotope('layout');
+    // filter items on button click
+    $('.work-filter').on('click', 'a', function (e) {
+        e.preventDefault();
+        var filterValue = $(this).attr('data-filter');
+        $container.isotope({ filter: filterValue });
+
+        $('.work-filter li').removeClass('active');
+        $(this).closest('li').addClass('active');
+    });
+
+    
 });
